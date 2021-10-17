9a7340fb51668d8d07cfd63980c2316dd428aa1a
edwinstanic <edwinstanic22@gmail.com>
2021-10-17 23:17:55 +0700

    Penambahan fungsi pengecekan hasil hash password dan komentar

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 fake_users_db = {
-    "admin": {
+    "asdf": {
         "username": "asdf",
         "full_name": "Admin",
         "email": "ii3160admin@gmail.com",
@@ -64,6 +64,7 @@ def authenticate_user(fake_db, username: str, password: str):
         return False
     return user
 
+# Token Creation
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
@@ -98,6 +99,7 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
+# Login
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
@@ -113,10 +115,17 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+# Read User Information
 @app.get("/users/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
+# Read User's Item
 @app.get("/users/me/items/")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
+
+# Read Hash Result
+@app.get("/pass")
+async def get_hash(password: str):
+    return pwd_context.hash(password)
